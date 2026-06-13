81b52446307d50e638d524be3ac9c7a7e34d2385
nhan <nguyennhann1109@gmail.com>
2026-06-13 17:37:05 +0700

    khoi tao

---
+++ apps/backend-realtime/src/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const connectionHandler = require("./sockets/connection");
+
+const app = express();
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "*"
+    }
+});
+
+connectionHandler(io);
+
+app.get("/", (req, res) => {
+    res.send("Backend Realtime Running");
+});
+
+const PORT = 3000;
+
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
